7874fa9edf81b269a75eee4a964ddc45605f49d0
H-AbdulMateen <abdulmateen6.ch@gmail.com>
2019-07-10 08:03:29 +0500

    testing

--- app/src/main/java/com/isofttechpro/mvvmsampleapp/ui/home/HomeActivity.kt
+++ app/src/main/java/com/isofttechpro/mvvmsampleapp/ui/home/HomeActivity.kt
@@ -3,13 +3,13 @@ package com.isofttechpro.mvvmsampleapp.ui.home
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.isofttechpro.mvvmsampleapp.R
+import com.isofttechpro.mvvmsampleapp.utils.toast
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-
-
+        toast("Welcome to MVVM Architecture Sample")
     }
 }

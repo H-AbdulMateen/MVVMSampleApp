93a4a3f74981e05e4890a43a54cea1af60aad0f1
H-AbdulMateen <abdulmateen6.ch@gmail.com>
2019-07-11 08:48:18 +0500

    Kodein Framework

--- app/src/main/java/com/isofttechpro/mvvmsampleapp/ui/auth/LoginActivity.kt
+++ app/src/main/java/com/isofttechpro/mvvmsampleapp/ui/auth/LoginActivity.kt
@@ -1,11 +1,9 @@
 package com.isofttechpro.mvvmsampleapp.ui.auth
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.isofttechpro.mvvmsampleapp.R
@@ -19,12 +17,16 @@ import com.isofttechpro.mvvmsampleapp.ui.home.HomeActivity
 import com.isofttechpro.mvvmsampleapp.utils.hide
 import com.isofttechpro.mvvmsampleapp.utils.show
 import com.isofttechpro.mvvmsampleapp.utils.snackbar
-import com.isofttechpro.mvvmsampleapp.utils.toast
 import kotlinx.android.synthetic.main.activity_login.*
-import kotlinx.android.synthetic.main.activity_sign_up.*
 import kotlinx.android.synthetic.main.activity_sign_up.progress_bar
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.kodein
+import org.kodein.di.generic.instance
 
-class LoginActivity : AppCompatActivity() , AuthListener {
+class LoginActivity : AppCompatActivity() , AuthListener , KodeinAware {
+
+    override val kodein by kodein()
+    private val factory : AuthViewModelFactory by instance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
